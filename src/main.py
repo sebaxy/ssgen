a6560158489c7024ac78acf969d8b8d6ae42abe6
sebaxy <seba.domanski@gmail.com>
2025-04-13 18:21:13 +0000

    add TextNode class

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import *
+
+def main():
+    test_tn = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(test_tn)
+
+if __name__ == "__main__":
+    main()
